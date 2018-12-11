17ccf76556dd0013d65abaadb2f714272708503e
dani832m <31951102+dani832m@users.noreply.github.com>
2018-12-11 09:03:36 +0100

    yoloSwaGz

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-//connect to MongoDB
+// Connect to MongoDB
 mongoose.connect('mongodb://admin:test12345@ds129914.mlab.com:29914/skraldespand_db',{useNewUrlParser: true,});
 var db = mongoose.connection;
 
-//handle mongo error
+// Handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('jeg er inde !!!')
-  // we're connected!
+  // We're connected!
 });
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -35,18 +35,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
